f542c270b2f13d4d7831aee4dfbd32442fc52ae4
Martin Andrieux <martin.andrieux@ens-rennes.fr>
2024-01-19 12:01:53 +0100

    closer to reality

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, abort
 from flask_socketio import SocketIO
+from datetime import date, timedelta
 
-from models import Cordes, ControlesCordes
+from models import EPI, Genre, Stockage, Controle, Rebut
 from models import db
-from forms import AjoutCordeForm, ControleCordeForm
+import forms
 
 import sys
 
@@ -21,7 +22,29 @@ socketio = SocketIO(app)
 if len(sys.argv) >= 2 and sys.argv[1] == "create-database":
     with app.app_context():
         db.create_all()
-    print("[INFO] Database created.")
+        print("[INFO] Database created.")
+
+        # Fill `Genre` table
+        db.session.add(Genre(gen_name="Harnais"))
+        db.session.add(Genre(gen_name="Corde"))
+        db.session.add(Genre(gen_name="Casque"))
+        db.session.add(Genre(gen_name="Anneau"))
+        db.session.add(Genre(gen_name="Assureur"))
+        db.session.add(Genre(gen_name="Degaine"))
+        db.session.add(Genre(gen_name="Mousqueton"))
+        db.session.add(Genre(gen_name="Bloqueur"))
+        db.session.add(Genre(gen_name="Coinceur"))
+
+        # Fill `Stockage` table
+        db.session.add(Stockage(sto_name="Local Félix"))
+        db.session.add(Stockage(sto_name="Caisse cours jeunes/débutants"))
+        db.session.add(Stockage(sto_name="Caisse maintenance"))
+        db.session.add(Stockage(sto_name="Caisse Sortie"))
+        db.session.add(Stockage(sto_name="Sac cours enfants/jeunes"))
+        db.session.add(Stockage(sto_name="Sac Handisport"))
+
+        db.session.commit()
+        print("[INFO] Database populated.")
     exit(0)
 
 
@@ -33,91 +56,122 @@ def home():
     return render_template("pages/index.html")
 
 
-@app.route("/ajout")
+@app.route("/ajout", methods=["GET", "POST"])
 def ajout():
-    return render_template("pages/ajout.html")
-
-
-@app.route("/ajout/corde", methods=["GET", "POST"])
-def ajout_corde():
-    form = AjoutCordeForm(request.form)
+    form = forms.AjoutMateriel(request.form)
     if request.method == "POST" and form.validate():
-        corde = Cordes(
-            fabricant=form.fabricant.data,
-            modele=form.modele.data,
-            serie=form.serie.data,
-            longueur=form.longueur.data,
-            couleur=form.couleur.data,
-            utilisation=form.utilisation.data,
-            description=form.description.data,
-            date_utilisation=form.date_utilisation.data,
-            date_fin_vie=form.date_fin_vie.data,
+        epi = EPI(
+            epi_genre=form.genre.data,
+            epi_type=form.ftype.data,
+            epi_marque=form.marque.data,
+            epi_modele=form.modele.data,
+            epi_quantite=form.quantite.data,
+            epi_marquage=form.marquage.data,
+            epi_stockage=form.stockage.data,
+            epi_date_fabrication=form.date_fabrication.data,
+            epi_date_utilisation=form.date_utilisation.data,
+            epi_duree_vie=form.duree_vie.data,
         )
-        db.session.add(corde)
-        db.session.commit()
-        return redirect("/")
-    return render_template("pages/ajout_corde.html", form=form)
-
-
-@app.route("/controle/corde/<int:corde_id>", methods=["GET", "POST"])
-def controle_corde_id(corde_id):
-    form = ControleCordeForm(request.form)
-    if request.method == "POST" and form.validate():
-        controle = ControlesCordes(
-            corde_id=corde_id,
-            date=form.date.data,
-            remarques=form.remarques.data,
-            usure=form.usure.data,
-            brulures=form.brulures.data,
-            coupures=form.coupures.data,
-            peluches=form.peluches.data,
-            ame=form.ame.data
+        db.session.add(epi)
+        # Ajout automatique d'un contrôle
+        db.session.flush()  # récupération de ei.epi_id
+        db.session.refresh(epi)
+        controle = Controle(
+            con_epi_id=epi.epi_id,
+            con_date=form.date_utilisation.data,
         )
         db.session.add(controle)
         db.session.commit()
-        return redirect(f"/recherche/corde/{corde_id}")
-    return render_template("pages/controle_corde.html", corde_id=corde_id, form=form)
+        return redirect("/")
+    return render_template("pages/ajout.html", form=form)
 
 
-@app.route("/recherche")
-def recherche():
-    return render_template("pages/recherche.html")
-
-
-@app.route("/recherche/corde")
-def recherche_corde():
+@app.route("/recherche/genre")
+def recherche_genre():
     data = []
-    for corde in Cordes.query.all():
+    for genre in Genre.query.all():
+        epis = []
+        for epi in EPI.query.filter(EPI.epi_genre == genre):
+            fin_de_vie = None
+            urgence = "all good"
+            if epi.epi_duree_vie != None:
+                fin_de_vie = epi.epi_date_utilisation + timedelta(
+                    days=365 * epi.epi_duree_vie
+                )
+                if fin_de_vie.year - date.today().year <= 2:
+                    urgence = "worrying"
+                if fin_de_vie.year - date.today().year <= 1:
+                    urgence = "critical"
+                fin_de_vie = fin_de_vie.strftime("%Y")
+            epis.append(
+                {
+                    "id": epi.epi_id,
+                    "type": epi.epi_type,
+                    "marque": epi.epi_marque,
+                    "modele": epi.epi_modele,
+                    "count": epi.epi_quantite,
+                    "marquage": epi.epi_marquage,
+                    "stockage": epi.epi_stockage.sto_name,
+                    "fin_de_vie": fin_de_vie,
+                    "urgence": urgence,
+                }
+            )
         data.append(
             {
-                "id": corde.id,
-                "modele": corde.modele,
-                "serie": corde.serie,
-                "couleur": corde.couleur,
-                "longueur": corde.longueur,
+                "genre": genre.gen_name,
+                "epis": epis,
             }
         )
-    return render_template("pages/recherche_corde.html", data=data)
+    return render_template("pages/recherche_genre.html", data_by_genre=data)
 
 
-@app.route("/recherche/corde/<int:corde_id>")
-def recherche_corde_id(corde_id):
-    corde = Cordes.query.get({"id": corde_id})
-    if corde is None:
+@app.route("/epi/<int:epi_id>", methods=["GET", "POST"])
+def epi(epi_id):
+    """
+    Interface de gestion d'un lot d'EPI.
+    - Toutes les informations relative à/au lot d'EPI.
+    - Stockage: visiter et déplacer
+    - Contrôles: liste, ajouter et mettre au rebut
+    """
+    if request.method == "POST":
+        if "rebut" in request.form.keys():
+            rebut = Rebut(
+                reb_epi_id=epi_id,
+                reb_date=date.today(),
+            )
+            db.session.add(rebut)
+            db.session.commit()
+        elif "controle" in request.form.keys():
+            controle = Controle(
+                con_epi_id=epi_id,
+                con_date=date.today(),
+            )
+            db.session.add(controle)
+            db.session.commit()
+        return redirect(f"/epi/{epi_id}")
+
+    epi = EPI.query.get({"epi_id": epi_id})
+    if epi is None:
         abort(404)
 
-    data = []
-    for controle in ControlesCordes.query.filter(ControlesCordes.corde_id == corde_id).order_by(ControlesCordes.date):
-        data.append({"date": controle.date.strftime("%d/%m/%Y")})
+    # REBUT
+    dernier_rebut = Rebut.query.get({"reb_epi_id": epi_id})
+
+    # CONTROLES
+    controles = []
+    for controle in Controle.query.where(Controle.con_epi_id == epi_id):
+        controles.append({"date": controle.con_date})
+
     return render_template(
-        "pages/recherche_corde_id.html",
-        corde_id=corde_id,
-        fabricant=corde.fabricant,
-        modele=corde.modele,
-        serie=corde.serie,
-        couleur=corde.couleur.lower(),
-        longueur=corde.longueur,
-        data=data,
+        "pages/epi.html",
+        epi_id=epi_id,
+        marque=epi.epi_marque,
+        modele=epi.epi_modele,
+        marquage="(" + epi.epi_marquage + ")" if epi.epi_marquage != "" else "",
+        stockage=epi.epi_stockage.sto_name,
+        has_rebut_date=dernier_rebut is not None,
+        rebut_date=dernier_rebut,
+        controles=controles,
     )
 
 
--- models.py
+++ models.py
@@ -1,33 +1,52 @@
 from datetime import date
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
-class Cordes(db.Model):
-    __tablename__ = "Cordes"
-    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    fabricant = db.Column(db.String(30))
-    modele = db.Column(db.String(30))
-    serie = db.Column(db.String(30))
-    longueur = db.Column(db.Integer)
-    couleur = db.Column(db.String(30))
-    utilisation = db.Column(db.String(20))
-    description = db.Column(db.String(50))
-    date_fin_vie = db.Column(db.Date)
-    date_utilisation = db.Column(db.DATE)
+class EPI(db.Model):
+    __tablename__ = "EPI"
+    epi_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    epi_genre_id = db.Column(db.Integer, db.ForeignKey("Genre.gen_id"), nullable=False)
+    epi_genre = db.relationship("Genre", backref=db.backref("EPI", lazy=True))
+    epi_type = db.Column(db.String(20))
+    epi_marque = db.Column(db.String(20), nullable=False)
+    epi_modele = db.Column(db.String(50))
+    epi_quantite = db.Column(db.Integer)
+    epi_marquage = db.Column(db.String(100))
+    epi_stockage_id = db.Column(
+        db.Integer, db.ForeignKey("Stockage.sto_id"), nullable=False
+    )
+    epi_stockage = db.relationship("Stockage", backref=db.backref("EPI", lazy=True))
+    epi_date_fabrication = db.Column(db.DATE)
+    epi_date_utilisation = db.Column(db.DATE)
+    epi_duree_vie = db.Column(db.Integer)
 
 
-class ControlesCordes(db.Model):
-    __tablename__ = "ControlesCordes"
-    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    corde_id = db.Column(db.Integer, db.ForeignKey("Cordes.id"), nullable=False)
-    corde = db.relationship("Cordes", backref=db.backref("ControlesCordes", lazy=True))
-    date = db.Column(db.DATE)
-    remarques = db.Column(db.String(50))
-    usure = db.Column(db.String(20))
-    brulures = db.Column(db.String(20))
-    coupures = db.Column(db.String(20))
-    peluches = db.Column(db.String(20))
-    ame = db.Column(db.String(20))
+class Genre(db.Model):
+    __tablename__ = "Genre"
+    gen_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    gen_name = db.Column(db.String(30))
+
+
+class Stockage(db.Model):
+    __tablename__ = "Stockage"
+    sto_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    sto_name = db.Column(db.String(100))
+
+
+class Controle(db.Model):
+    __tablename__ = "Controle"
+    con_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    con_epi_id = db.Column(db.Integer, db.ForeignKey("EPI.epi_id"), nullable=False)
+    con_epi = db.relationship("EPI", backref=db.backref("Controle", lazy=True))
+    con_date = db.Column(db.Date)
+
+
+class Rebut(db.Model):
+    __tablename__ = "Rebut"
+    reb_epi_id = db.Column(
+        db.Integer, db.ForeignKey("EPI.epi_id"), primary_key=True, nullable=False
+    )
+    reb_epi = db.relationship("EPI", backref=db.backref("Rebut", lazy=True))
+    reb_date = db.Column(db.DATE)
